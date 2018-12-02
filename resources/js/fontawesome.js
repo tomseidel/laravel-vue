fcefd9c2d2eddbc79160af45aaad5172f873bd82
Thomas Seidel <thomas@seidel.info>
2018-12-02 19:20:05 +0100

    Got favicons working

--- resources/js/fontawesome.js
+++ resources/js/fontawesome.js
@@ -1,7 +1,9 @@
-import fontawesome from '@fortawesome/fontawesome';
-import faCaretUp from '@fortawesome/fontawesome-free-solid/faCaretUp';
-import faCaretDown from '@fortawesome/fontawesome-free-solid/faCaretDown';
-import faStar from '@fortawesome/fontawesome-free-solid/faStar';
-import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-fontawesome.library.add([faCaretUp, faCaretDown, faCheck, faStar]);
+library.add([faCaretDown, faCaretUp, faCheck, faStar]);
+
+dom.watch();
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -18,19 +18,20 @@
                     <hr>
 
                     <div class="media">
+                        <div class="d-fex flex-column vote-controls">
+                            <a title="This question is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">1230</span>
+                            <a title="This question is not useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i>
+                                <span class="favorites-count">123</span>
+                            </a>
+                        </div>
                         <div class="media-body">
-                            <div class="d-fex flex-column vote-controls">
-                                <a title="This answer is useful" class="vote-up">
-                                    <i class="fas fa-caret-up fa-3x"></i>
-                                </a>
-                                <span class="votes-count">1230</span>
-                                <a title="This answer is not useful" class="vote-down off">
-                                    <i class="fas fa-caret-down fa-3x"></i>
-                                </a>
-                                <a title="Mark this answer as best answer" class="vote-accepted mt-2">
-                                    <i class="fas fa-check fa-2x"></i>                                    
-                                </a>
-                            </div>
                             {!! $question->body_html !!}
                             <div class="float-right">
                                 <span class="text-muted">Asked {{ $question->created_date }}</span>
@@ -59,7 +60,19 @@
                     </div>
                     <hr>
                     @foreach ($question->answers as $answer)
-                        <div class="media">
+                            <div class="media">
+                                <div class="d-fex flex-column vote-controls">
+                                <a title="This answer is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1230</span>
+                                <a title="This answer is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Mark this answer as bed answer" class="vote-accepted mt-2">
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                            </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
