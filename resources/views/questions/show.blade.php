4c891e7f216ff27a8765af8a73eef1c8a7ef8d79
Thomas Seidel <thomas@seidel.info>
2018-12-02 15:47:46 +0100

    Added show functionality

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>Create Question</h2>
+                        <h1>{{ $question->title }}</h1>
                         <div class="ml-auto">
                             <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                         </div>
@@ -15,9 +15,7 @@
                 </div>
 
                 <div class="card-body">
-                    <form action="{{ route('questions.store') }}" method="post">
-                        @include ('questions._form', ['buttonText' => 'Ask Question'])
-                    </form>
+                    {!! $question->body_html !!}
                 </div>
             </div>
         </div>
