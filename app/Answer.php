08ca776a20ba31d69d17f05ae0ba5191acdf3ac8
Thomas Seidel <thomas@seidel.info>
2018-12-02 17:50:16 +0100

    Display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -16,6 +16,11 @@ class Answer extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
