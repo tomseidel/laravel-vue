ec7953f47a8fa0a08e811ca408d5a1aeb6264371
Thomas Seidel <thomas@seidel.info>
2018-12-06 09:06:45 +0100

    Update best_answer_id on questions table by foreignkey

---
+++ database/migrations/2018_12_06_075430_add_foreign_answer_id_to_question_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignAnswerIdToQuestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->foreign('best_answer_id')
+                    ->references('id')
+                    ->on('answers')
+                    ->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropforeign(['best_answer_id']);
+        });
+    }
+}
