722ba67e5234854d5b08baa2c4a13500faa98c46
Thomas Seidel <thomas@seidel.info>
2018-12-06 14:58:27 +0100

    Display all questions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersQuestionsanswersTableSeeder::class,
+            UsersQuestionsAnswersTableSeeder::class,
             FavoritesTableSeeder::class,
-            VotableTableSeeder::class,
+            VotablesTableSeeder::class,
         ]);
     }
 }
