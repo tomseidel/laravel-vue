99708eb9af7e66e9cf518f9134c69905bab92185
Thomas Seidel <thomas@seidel.info>
2018-12-06 09:48:24 +0100

    Implement accept answer

--- app/Answer.php
+++ app/Answer.php
@@ -43,7 +43,17 @@ class Answer extends Model
 
     public function getStatusAttribute()
     {
-        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
+        return $this->isBest() ? 'vote-accepted' : '';
+    }
+
+    public function isBest()
+    {
+        return $this->id == $this->question->best_answer_id;
+    }
+
+    public function getIsBestAttribute()
+    {
+        return $this->isBest();
     }
 
 }
